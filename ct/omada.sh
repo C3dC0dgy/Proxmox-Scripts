6cc6ebdab70187480072f7400ae744ccf4d7676d
C3dC0dgy <52158333+C3dC0dgy@users.noreply.github.com>
2024-11-27 01:26:55 -0500

    Update omada.sh

--- ct/omada.sh
+++ ct/omada.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+source <(curl -s https://raw.githubusercontent.com/C3dC0dgy/Proxmox-Scripts/main/misc/build.func)
 
 function header_info {
-clear
-cat <<"EOF"
+  clear
+  cat <<"EOF"
    ____                      __     
   / __ \____ ___  ____ _____/ /___ _
  / / / / __  __ \/ __  / __  / __  /
@@ -16,6 +12,7 @@ cat <<"EOF"
  
 EOF
 }
+
 header_info
 echo -e "Loading..."
 APP="Omada"
@@ -28,52 +25,33 @@ variables
 color
 catch_errors
 
-function default_settings() {
-  CT_TYPE="1"
-  PW=""
-  CT_ID=$NEXTID
-  HN=$NSAPP
-  DISK_SIZE="$var_disk"
-  CORE_COUNT="$var_cpu"
-  RAM_SIZE="$var_ram"
-  BRG="vmbr0"
-  NET="dhcp"
-  GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
-  VERB="no"
-  echo_default
+# Prompt for Omada version
+prompt_omada_version() {
+  echo -e "Please enter the Omada version you want to install (e.g., 5.9.31):"
+  read -p "Version: " OMADA_VERSION
+  if [[ -z "$OMADA_VERSION" ]]; then
+    msg_error "No version entered. Exiting."
+    exit 1
+  fi
 }
 
 function update_script() {
-header_info
-if [[ ! -d /opt/tplink ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+  header_info
+  if [[ ! -d /opt/tplink ]]; then 
+    msg_error "No ${APP} Installation Found!"; 
+    exit; 
+  fi
 
-# Specify the version of Omada you want to install
-SPECIFIED_VERSION="5.14.26.1" # Change this to your desired version
-base_url="https://static.tp-link.com"
-file_name="Omada_SDN_Controller_v${SPECIFIED_VERSION}_Linux_x64.deb"
-specified_url="${base_url}/${file_name}"
+  # Prompt for the version and build the URL
+  prompt_omada_version
+  build_omada_url
+  download_omada_version
 
-echo -e "Downloading Omada Controller version ${SPECIFIED_VERSION}"
-wget -qL ${specified_url}
-if [ $? -ne 0 ]; then
-  msg_error "Failed to download version ${SPECIFIED_VERSION}. Please check the version and try again."
-  exit
-fi
-
-echo -e "Installing Omada Controller version ${SPECIFIED_VERSION}"
-dpkg -i ${file_name}
-rm -rf ${file_name}
-echo -e "Installed Omada Controller version ${SPECIFIED_VERSION}"
-exit
+  # Install the specified version
+  echo -e "Installing Omada Controller version ${OMADA_VERSION}..."
+  dpkg -i "Omada_SDN_Controller_v${OMADA_VERSION}_Linux_x64.deb"
+  rm -f "Omada_SDN_Controller_v${OMADA_VERSION}_Linux_x64.deb"
+  msg_ok "Installed Omada Controller version ${OMADA_VERSION}."
 }
 
 start
@@ -81,5 +59,5 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} should be reachable by going to the following URL.
+echo -e "${APP} should be reachable by going to the following URL:
          ${BL}https://${IP}:8043${CL} \n"
